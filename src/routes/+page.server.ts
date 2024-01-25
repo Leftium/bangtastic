91e95f9a4308962c48029fc148e0f4ab3eef48a4
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-01-26 07:18:10 +0900

    Move bang processing logic to page.server.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,106 @@
+import _ from 'lodash';
+
+import bangData from '$lib/bang-data/bang.json';
+
+import normalizeUrl from 'normalize-url';
+
+const uniqueUrls: Record<string, any> = {};
+const uniqueUrlsNormalized: Record<string, any> = {};
+
+function normalize(url: string) {
+	if (url[0] === '/') {
+		url = 'duckduckgo.com' + url;
+	}
+	url = url.toLocaleLowerCase().replace('{{{s}}}', 'QUERY');
+	return normalizeUrl(url, {
+		stripProtocol: true
+	});
+}
+
+const augmentedBangs = _.chain(bangData)
+	.map((bang) => {
+		const normalizedUrl = normalize(bang.u);
+		(uniqueUrlsNormalized[normalizedUrl] = uniqueUrlsNormalized[normalizedUrl] || []).push(bang.t);
+
+		(uniqueUrls[bang.u] = uniqueUrls[bang.u] || []).push(bang.t);
+
+		return bang;
+	})
+	.map((bang) => {
+		const normalizedUrl = normalize(bang.u);
+		return {
+			tLength: bang.t.length,
+			uCountNormalized: uniqueUrlsNormalized[normalizedUrl].length,
+			uCount: uniqueUrls[bang.u].length,
+			uNormalized: normalizedUrl,
+			...bang
+		};
+	})
+	//.filter((bang) => bang.uCountNormalized > 1)
+	.orderBy(['uCountNormalized', 'r', 'tLength'], ['desc', 'desc', 'desc'])
+	.value();
+
+// https://stackoverflow.com/a/74581333/117030
+const bangsNormalized = Object.values(
+	augmentedBangs.reduce(
+		(x, y) => {
+			(x[y.uNormalized] = x[y.uNormalized] || [
+				{ tMin: y.tLength, tMax: y.tLength, t: '', u: '', s: '' }
+			]).push(y);
+
+			const merged = x[y.uNormalized][0];
+			if (merged) {
+				merged.tMax = Math.max(merged.tMax, y.tLength);
+				merged.tMin = Math.min(merged.tMin, y.tLength);
+			}
+
+			return x;
+		},
+		{} as Record<string, any>
+	)
+).map((bangs) => {
+	// triggers: shortest, longest, highest score, rest
+	const shortest = _.find(bangs, ['tLength', bangs[0].tMin]).t;
+	const longest = _.find(bangs, ['tLength', bangs[0].tMax]).t;
+
+	const t = _.uniq(_.concat(shortest, longest, _.map(bangs, 't'))).join(' ');
+
+	bangs[0].t = t;
+
+	const u = _.chain(bangs)
+		.map('u')
+		.sortBy([(u) => u.length, (u) => /^https/.test(u)])
+		.value()[1]
+		.replace('{{{s}}}', '{s}');
+
+	const s = _.chain(bangs)
+		.map('s')
+		.sortBy([(u) => -u.length])
+		.value()[0];
+
+	const { r, c, sc } = _.chain(bangs)
+		.map(({ r, c, sc }) => ({ r, c, sc }))
+		.orderBy(['r'], ['desc'])
+		.value()[1];
+
+	bangs[0] = {
+		...bangs[0],
+		t,
+		u,
+		s,
+		r,
+		c,
+		sc
+	};
+	return bangs;
+});
+
+const bangs = _.chain(bangsNormalized)
+	.map((bangs) => bangs[0])
+	.orderBy(['r'], ['desc'])
+	.map(({ t, u, r, s, c, sc }) => ({ t, u, s }))
+	.value();
+
+export const load = async () => {
+	return { bangs };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,110 +1,7 @@
 <script lang="ts">
 	import '@picocss/pico';
 
-	import _ from 'lodash';
-
-	import bangData from '$lib/bang-data/bang.json';
-
-	import normalizeUrl from 'normalize-url';
-
-	const uniqueUrls: Record<string, any> = {};
-	const uniqueUrlsNormalized: Record<string, any> = {};
-
-	function normalize(url: string) {
-		if (url[0] === '/') {
-			url = 'duckduckgo.com' + url;
-		}
-		url = url.toLocaleLowerCase().replace('{{{s}}}', 'QUERY');
-		return normalizeUrl(url, {
-			stripProtocol: true
-		});
-	}
-
-	const bangs = _.chain(bangData)
-		.map((bang) => {
-			const normalizedUrl = normalize(bang.u);
-			(uniqueUrlsNormalized[normalizedUrl] = uniqueUrlsNormalized[normalizedUrl] || []).push(
-				bang.t
-			);
-
-			(uniqueUrls[bang.u] = uniqueUrls[bang.u] || []).push(bang.t);
-
-			return bang;
-		})
-		.map((bang) => {
-			const normalizedUrl = normalize(bang.u);
-			return {
-				tLength: bang.t.length,
-				uCountNormalized: uniqueUrlsNormalized[normalizedUrl].length,
-				uCount: uniqueUrls[bang.u].length,
-				uNormalized: normalizedUrl,
-				...bang
-			};
-		})
-		//.filter((bang) => bang.uCountNormalized > 1)
-		.orderBy(['uCountNormalized', 'r', 'tLength'], ['desc', 'desc', 'desc'])
-		.value();
-
-	// https://stackoverflow.com/a/74581333/117030
-	const bangsNormalized = Object.values(
-		bangs.reduce(
-			(x, y) => {
-				(x[y.uNormalized] = x[y.uNormalized] || [
-					{ tMin: y.tLength, tMax: y.tLength, t: '', u: '', s: '' }
-				]).push(y);
-
-				const merged = x[y.uNormalized][0];
-				if (merged) {
-					merged.tMax = Math.max(merged.tMax, y.tLength);
-					merged.tMin = Math.min(merged.tMin, y.tLength);
-				}
-
-				return x;
-			},
-			{} as Record<string, any>
-		)
-	).map((bangs) => {
-		// triggers: shortest, longest, highest score, rest
-		const shortest = _.find(bangs, ['tLength', bangs[0].tMin]).t;
-		const longest = _.find(bangs, ['tLength', bangs[0].tMax]).t;
-
-		const t = _.uniq(_.concat(shortest, longest, _.map(bangs, 't'))).join(' ');
-
-		bangs[0].t = t;
-
-		const u = _.chain(bangs)
-			.map('u')
-			.sortBy([(u) => u.length, (u) => /^https/.test(u)])
-			.value()[1]
-			.replace('{{{s}}}', '{s}');
-
-		const s = _.chain(bangs)
-			.map('s')
-			.sortBy([(u) => -u.length])
-			.value()[0];
-
-		const { r, c, sc } = _.chain(bangs)
-			.map(({ r, c, sc }) => ({ r, c, sc }))
-			.orderBy(['r'], ['desc'])
-			.value()[1];
-
-		bangs[0] = {
-			...bangs[0],
-			t,
-			u,
-			s,
-			r,
-			c,
-			sc
-		};
-		return bangs;
-	});
-
-	const outputBangs = _.chain(bangsNormalized)
-		.map((bangs) => bangs[0])
-		.orderBy(['r'], ['desc'])
-		.map(({ t, u, r, s, c, sc }) => ({ t, u, s }))
-		.value();
+	export let data;
 
 	function onkeydown(this: HTMLInputElement, event: Event) {
 		const e = event as KeyboardEvent;
@@ -156,7 +53,7 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each outputBangs as bang, index (bang.t)}
+			{#each data.bangs as bang, index (bang.t)}
 				<tr>
 					<td>{index}</td>
 					{#each columns as columnName}
@@ -168,7 +65,7 @@
 	</table>
 
 	{#if false}
-		{#each bangsNormalized as bangs}
+		{#each data.bangs as bangs}
 			<table>
 				<thead>
 					<tr>
