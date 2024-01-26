17e156d47fcffddc0b893a02107df3964790067e
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-01-26 15:05:40 +0900

    API: add params for format and protocol.

--- src/routes/api/bangs/+server.ts
+++ src/routes/api/bangs/+server.ts
@@ -1,6 +1,24 @@
 import { bangs } from '$lib/bang';
-import { json } from '@sveltejs/kit';
+import { text, json } from '@sveltejs/kit';
 
-export const GET = async () => {
-	return json(bangs);
+export const GET = async ({ url }) => {
+	const format = url.searchParams.get('format') || url.searchParams.get('f');
+	const includeProtocol = ['1', 'true'].includes(
+		url.searchParams.get('protocol') || url.searchParams.get('p') || 'true'
+	);
+
+	let bangsOutput = bangs;
+
+	if (!includeProtocol) {
+		bangsOutput = bangs.map((bang) => ({
+			...bang,
+			u: bang.u.replace(/^https?:\/\//, '')
+		}));
+	}
+
+	if (format === 'text') {
+		return text(bangsOutput.map(({ t, u, s }) => `${t}\n${u}\n${s}\n`).join('\n'));
+	}
+
+	return json(bangsOutput);
 };
