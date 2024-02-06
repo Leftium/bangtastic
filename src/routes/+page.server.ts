451c41410411344dc47e689d1908093d4ae2f902
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-02-06 14:34:06 +0900

    Forward request to Kagi if contains q param.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,8 @@
-export const load = async () => {};
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ url }) => {
+	const q = url.searchParams.get('q');
+	if (q) {
+		redirect(302, `https://kagi.com/search?q=${q}`);
+	}
+};
